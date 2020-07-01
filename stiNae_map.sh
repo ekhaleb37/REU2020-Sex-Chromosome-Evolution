58778692d0da76009932d4dee97c9a0a6277a6d1
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-01 14:40:10 -0400

    Update stiNae_map.sh

--- stiNae_map.sh
+++ stiNae_map.sh
@@ -10,7 +10,7 @@
 module load bwa/0.7.17-fasrc01
 
 R1=$1
-name=`echo $R1 | sed 's/_1.fastq.gz\+//'`
-R2=${name}_2.fastq.gz
+name=`echo $R1 | sed 's/_R1.fastq.gz\+//'`
+R2=${name}_R2.fastq.gz
 #run mapping
 bwa mem -t 8 -R '@RG\tID:${name}\tSM:${name}' stiNae_index $R1 $R2 > $name.sam
