0d3f23c2619d345169b205174fe897a52a2d48b1
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-07-01 12:29:32 -0400

    Create 01_makerefs.sh

---
+++ 01_makerefs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 3000
+#SBATCH -t 180
+#SBATCH -o makerefs.%N.%j.out
+#SBATCH -e makerefs.%N.%j.err
+
+module load bwa/0.7.17-fasrc01
+
+bwa index -p netAur_index ../swuitchik/summer2020REU/duck_data/netAur/refGenome/GCA_011076525.1_BPBGC_Naur_1.0_genomic.fna.gz
+bwa index -p oxyJam_index ../swuitchik/summer2020REU/duck_data/oxyJam/refGenome/GCA_011077185.1_BPBGC_Ojam_1.0_genomic.fna.gz
+bwa index -p stiNae_index ../swuitchik/summer2020REU/duck_data/stiNae/refGenome/GCA_011074415.1_BPBGC_Snae_1.0_genomic.fna.gz
